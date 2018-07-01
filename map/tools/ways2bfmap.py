aa12eb0c615ccd564cd5df73ec0a2480c21f4e05
Frank <33519926+Conengmo@users.noreply.github.com>
2018-07-01 18:22:04 +0200

    Overhaul of bfmap scripts

--- map/tools/ways2bfmap.py
+++ map/tools/ways2bfmap.py
@@ -19,99 +19,121 @@ __license__ = "Apache-2.0"
 
 import optparse
 import getpass
+import json
+
 import bfmap
+import common
 
-parser = optparse.OptionParser("ways2bfmap.py [options]")
-parser.add_option(
-    "--source-host", dest="source_host", help="Hostname of the source database.")
-parser.add_option(
-    "--source-port", dest="source_port", help="Port of the source database.")
-parser.add_option("--source-database", dest="source_database",
-                  help="Name of the source database.")
-parser.add_option(
-    "--source-table", dest="source_table", help="Name of the source table.")
-parser.add_option(
-    "--source-user", dest="source_user", help="User of source database.")
-parser.add_option("--source-password", dest="source_password",
-                  help="User password of source database.")
-parser.add_option(
-    "--target-host", dest="target_host", help="Hostname of the target database.")
-parser.add_option(
-    "--target-port", dest="target_port", help="Port of the target database.")
-parser.add_option("--target-database", dest="target_database",
-                  help="Name of the target database.")
-parser.add_option(
-    "--target-table", dest="target_table", help="Name of the target table.")
-parser.add_option(
-    "--target-user", dest="target_user", help="User of target database.")
-parser.add_option("--target-password", dest="target_password",
-                  help="User password of target database.")
-parser.add_option("--config", dest="config",
-                  help="Configuration file for OSM data interpretation. (XML)")
-parser.add_option("--append", action="store_true",
-                  default=False, help="Append data if target table exists.")
-parser.add_option("--printonly", action="store_true",
-                  default=False, help="Do not execute commands, but print it.")
 
-(options, args) = parser.parse_args()
+def get_command_line_options():
+    parser = optparse.OptionParser("ways2bfmap.py [options]")
+    parser.add_option(
+        "--source-host", dest="source_host", help="Hostname of the source database.")
+    parser.add_option(
+        "--source-port", dest="source_port", help="Port of the source database.")
+    parser.add_option("--source-database", dest="source_database",
+                      help="Name of the source database.")
+    parser.add_option(
+        "--source-table", dest="source_table", help="Name of the source table.")
+    parser.add_option(
+        "--source-user", dest="source_user", help="User of source database.")
+    parser.add_option("--source-password", dest="source_password",
+                      help="User password of source database.")
+    parser.add_option(
+        "--target-host", dest="target_host", help="Hostname of the target database.")
+    parser.add_option(
+        "--target-port", dest="target_port", help="Port of the target database.")
+    parser.add_option("--target-database", dest="target_database",
+                      help="Name of the target database.")
+    parser.add_option(
+        "--target-table", dest="target_table", help="Name of the target table.")
+    parser.add_option(
+        "--target-user", dest="target_user", help="User of target database.")
+    parser.add_option("--target-password", dest="target_password",
+                      help="User password of target database.")
+    parser.add_option("--config", dest="config",
+                      help="Configuration file for OSM data interpretation. (XML)")
+    parser.add_option("--append", action="store_true",
+                      default=False, help="Append data if target table exists.")
+    parser.add_option("--printonly", action="store_true",
+                      default=False, help="Do not execute commands, but print it.")
 
-if options.source_host == None or \
-        options.source_port == None or \
-        options.source_database == None or \
-        options.source_table == None or \
-        options.source_user == None or \
-        options.target_host == None or \
-        options.target_port == None or \
-        options.target_database == None or \
-        options.target_table == None or \
-        options.target_user == None or \
-        options.config == None:
-    parser.print_help()
-    exit(1)
+    (options, args) = parser.parse_args()
 
-if options.source_password == None:
-    source_password = getpass.getpass("Password (source database):")
-else:
-    source_password = options.source_password
+    if (options.source_host is None
+            or options.source_port is None
+            or options.source_database is None
+            or options.source_table is None
+            or options.source_user is None
+            or options.target_host is None
+            or options.target_port is None
+            or options.target_database is None
+            or options.target_table is None
+            or options.target_user is None
+            or options.config is None):
+        parser.print_help()
+        exit(1)
 
-if options.target_password == None:
-    target_password = getpass.getpass("Password (target database):")
-else:
-    target_password = options.target_password
+    if options.source_password is None:
+        source_password = getpass.getpass("Password (source database):")
+    else:
+        source_password = options.source_password
 
-config = bfmap.config(options.config)
-print("Configuration imported.")
+    if options.target_password is None:
+        target_password = getpass.getpass("Password (target database):")
+    else:
+        target_password = options.target_password
 
-if not bfmap.exists(options.target_host, options.target_port, options.target_database,
-                    options.target_table, options.target_user, target_password):
-    print("Table '%s' does not exist in database '%s'." % 
-          (options.target_table, options.target_database))
-    bfmap.schema(options.target_host, options.target_port, options.target_database,
-                options.target_table, options.target_user, target_password, options.printonly)
-    print("Table '%s' has been created." % options.target_table)
-else:
-    print("Table '%s' already exists in database '%s'." % 
-          (options.target_table, options.target_database))
-    if not options.append:
-        while True:
-            value = raw_input(
-                            "Do you want to remove table '%s' (y/n)?: " % options.target_table).lower()
-            if value == 'n':
-                print("Append data to table '%s'." % options.target_table)
-                break
-            elif value == 'y':
-                bfmap.remove(options.target_host, options.target_port, options.target_database,
-                            options.target_table, options.target_user, target_password, options.printonly)
-                print("Table '%s' has been removed." % options.target_table)
-                bfmap.schema(options.target_host, options.target_port, options.target_database,
-                            options.target_table, options.target_user, target_password, options.printonly)
-                print("Table '%s' has been recreated." % options.target_table)
-                break
+    return options, source_password, target_password
 
-print("Inserting data ...")
-bfmap.ways2bfmap(options.source_host, options.source_port, options.source_database,
-                 options.source_table, options.source_user, source_password,
-                 options.target_host, options.target_port, options.target_database,
-                 options.target_table, options.target_user, target_password, config,
-                 options.printonly)
-print("Done.")
+
+def read_config_file(file):
+    """Read the config file with the road types."""
+    with open(file) as f:
+        jsondata = json.load(f)
+    config = {}
+    for tag in jsondata["tags"]:
+        tagconfig = {}
+        for value in tag["values"]:
+            tagconfig[value["name"]] = (value["id"],
+                                        value["priority"],
+                                        value["maxspeed"])
+        config[tag["tag"]] = tagconfig
+    return config
+
+
+def main():
+    options, source_password, target_password = get_command_line_options()
+    config = read_config_file(options.config)
+    print("Configuration imported.")
+    db = bfmap.Bfmap(options.target_host, options.target_port, options.target_database,
+                     options.target_user, target_password, options.printonly)
+    if not db.exists(options.target_table):
+        print("Table '{}' does not exist in database '{}'."
+              .format(options.target_table, options.target_database))
+        db.create_schema(options.target_table)
+        print("Table '{}' has been created.".format(options.target_table))
+    else:
+        if options.append:
+            print("Table '%s' already exists in database '%s'." %
+                  (options.target_table, options.target_database))
+        else:
+            if not common.get_confirmation_for_table_deletion(options.target_table,
+                                                              options.target_database):
+                print("Append data to table '{}'.".format(options.target_table))
+            else:
+                db.drop(options.target_table)
+                print("Table '{}' has been removed.".format(options.target_table))
+                db.create_schema(options.target_table)
+                print("Table '{}' has been created.".format(options.target_table))
+
+    print("Inserting data ...")
+    db.ways2bfmap(options.source_host, options.source_port, options.source_database,
+                  options.source_table, options.source_user, source_password,
+                  options.target_table, config)
+    db.close()
+    print("Done.")
+
+
+if __name__ == '__main__':
+    main()
