e43b5bf2067c405e18d7cf2b139447a1620ce338
NolanChan1 <57039901+NolanChan1@users.noreply.github.com>
2021-04-18 19:38:31 -0600

    Added RoadMapReader and RoadReaderFactory
    These classes implement a factory design pattern.

---
+++ src/main/java/com/bmwcarit/barefoot/road/RoadMapReader.java
@@ -0,0 +1,78 @@
+package com.bmwcarit.barefoot.road;
+
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Iterator;
+
+import com.esri.core.geometry.Polygon;
+import com.esri.core.geometry.SpatialReference;
+import com.esri.core.geometry.GeometryEngine;
+
+import com.bmwcarit.barefoot.roadmap.RoadMap;
+import com.bmwcarit.barefoot.util.SourceException;
+
+/**
+ * Barefoot map reader for reading BaseRoad objects from a supplied RoadMap object.
+ * Used to get BaseRoad data from file buffer.
+ */
+public class RoadMapReader implements RoadReader{
+	private RoadMap readerSrc;
+	
+	private Iterator<Road> iterator;
+	private HashSet<Short> exclusions;
+	private Polygon polygon;
+	
+	public RoadMapReader(RoadMap src) {
+		iterator = null;
+		exclusions = null;
+		polygon = null;
+		
+		readerSrc = src;
+	}
+	
+	@Override
+    public boolean isOpen() {
+        return (iterator != null);
+    }
+
+    @Override
+    public void open() throws SourceException {
+        open(null, null);
+    }
+
+    @Override
+    public void open(Polygon polygon, HashSet<Short> exclusions) throws SourceException {
+        iterator = readerSrc.getEdges().values().iterator();
+        this.exclusions = exclusions;
+        this.polygon = polygon;
+    }
+
+    @Override
+    public void close() throws SourceException {
+        iterator = null;
+    }
+
+    @Override
+    public BaseRoad next() throws SourceException {
+        BaseRoad road = null;
+        do {
+            if (!iterator.hasNext()) {
+                return null;
+            }
+
+            Road _road = iterator.next();
+
+            if (_road.id() % 2 == 1) {
+                continue;
+            }
+
+            road = _road.base();
+        } while (road == null || exclusions != null && exclusions.contains(road.type())
+                || polygon != null
+                        && !GeometryEngine.contains(polygon, road.geometry(),
+                                SpatialReference.create(4326))
+                        && !GeometryEngine.overlaps(polygon, road.geometry(),
+                                SpatialReference.create(4326)));
+        return road;
+    }
+}
---
+++ src/main/java/com/bmwcarit/barefoot/road/RoadReaderFactory.java
@@ -0,0 +1,46 @@
+package com.bmwcarit.barefoot.road;
+
+import java.util.Map;
+
+import com.bmwcarit.barefoot.util.Tuple;
+import com.bmwcarit.barefoot.roadmap.RoadMap;
+
+public class RoadReaderFactory {
+	
+	/**
+	 * Creates a RoadReader that can read BaseRoad objects from a PostgreSQL/PostGIS database
+	 * 
+	 * @param host The hostname of the database server.
+	 * @param port The port number of the database server.
+	 * @param database The name of the database.
+	 * @param table The name of the table in the database to read from.
+	 * @param user The username of a user accessing the database.
+	 * @param password The password of a user accessing the database.
+	 * @param config Configuration of road type data for RoadReader to read
+	 * @return A RoadReader that reads BaseRoad objects from a PostgreSQL/PostGIS database (PostGISReader)
+	 */
+	public RoadReader createPostGISReader(String host, int port, String database, String table, String user,
+			String password, Map<Short, Tuple<Double, Integer>> config) {
+		return new PostGISReader(host, port, database, table, user, password, config)
+	}
+	
+	/**
+	 * Creates a RoadReader that can read BaseRoad objects from a barefoot map files (file extension 'bfmap')
+	 * 
+	 * @param path The path to the barefoot map file to read.
+	 * @return A RoadReader that reads BaseRoad objects from a barefoot map file.
+	 */
+	public RoadReader createBfmapReader(String path) {
+		return new BfmapReader(path);
+	}
+	
+	/**
+	 * Creates a RoadReader that reads BaseRoad objects from a RoadMap object.
+	 * 
+	 * @param src Source of BaseRoad objects to read from.
+	 * @return A RoadReader that reads BaseRoad objects from a RoadMap.
+	 */
+	public RoadReader createRoadMapReader(RoadMap src) {
+		return new RoadMapReader(src);
+	}
+}
